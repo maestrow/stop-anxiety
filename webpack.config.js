259050efd84841735effce32eaba569911f79e6d
maestrow <andrew.shmelev@gmail.com>
2018-03-25 00:04:07 +0300

    Fix webpack.config to build faster in dev mode.

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,146 @@
 const path = require("path");
-const fs = require("fs");
 const webpack = require("webpack");
 const fableUtils = require("fable-utils");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 function resolve(filePath) {
-  return path.join(__dirname, filePath)
+    return path.join(__dirname, filePath)
 }
 
 var babelOptions = fableUtils.resolveBabelOptions({
-  presets: [["es2015", { "modules": false }]],
-  plugins: ["transform-runtime"]
+    presets: [
+        ["env", {
+            "targets": {
+                "browsers": ["last 2 versions"]
+            },
+            "modules": false
+        }],
+        "react"
+    ],
+    plugins: [
+        "transform-class-properties"
+    ]
 });
 
-module.exports = (env, argv) => ({
-  devtool: "source-map",
-  entry:  {
-    //js: resolve('./src/app.js'),
-    fsproj: resolve('./src/StopAnxiety.fsproj'),
-  },
-  devServer: {
-    contentBase: resolve('dist'),
-    port: 8080
-  },
-  resolve: {
-    modules: [
-      "node_modules", resolve("./node_modules/")
-    ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.fs(x|proj)?$/,
-        use: {
-          loader: "fable-loader",
-          options: {
-            babel: babelOptions,
-            define: argv.mode === 'production' ? [] : ["DEBUG"]
-          }
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: babelOptions
-        },
-      },
-      {
-        test: /\.sass$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
-        ]
-      }
-    ]
-  },
-  plugins: [ 
+var commonPlugins = [
     new HtmlWebpackPlugin({
-      inject: 'body',
-      template: 'src/index.html'
+        filename: resolve('./output/index.html'),
+        template: resolve('./src/index.html')
     })
-  ],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /node_modules/,
-          chunks: 'initial',
-          name: 'vendor',
-          enforce: true
+];
+
+var isProduction = process.argv.indexOf("-p") >= 0;
+console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
+
+module.exports = {
+    mode: isProduction ? "production" : "development",
+    devtool: isProduction ? false : 'source-map',
+    entry: isProduction ? // We don't use the same entry for dev and production, to make HMR over style quicker for dev env
+        {
+            app: [
+                "babel-polyfill",
+                resolve('./src/StopAnxiety.fsproj'),
+                resolve('./src/styles.sass')
+            ]
+        } : {
+            app: [
+                "babel-polyfill",
+                resolve('./src/StopAnxiety.fsproj')
+            ],
+            style: [
+                resolve('./src/styles.sass')
+            ]
         },
-      }
-    } 
-  }
-});
+    output: {
+        path: resolve('./output'),
+        filename: isProduction ? '[name].[hash].js' : '[name].js'
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor',
+                    chunks: 'all'
+                },
+                fable: {
+                    test: /[\\/]fable-core[\\/]/,
+                    name: 'fable',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    plugins: isProduction ?
+        commonPlugins.concat([
+            new ExtractTextPlugin('style.[contenthash].css'),
+            new CopyWebpackPlugin([
+                { from: './public' }
+            ]),
+            // ensure that we get a production build of any dependencies
+            // this is primarily for React, where this removes 179KB from the bundle
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': '"production"'
+            })
+        ])
+        : commonPlugins.concat([
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NamedModulesPlugin()
+        ]),
+    resolve: {
+        modules: [ 
+            "node_modules", resolve("./node_modules/") 
+        ]
+    },
+    devServer: {
+        //contentBase: resolve('./public/'),
+        publicPath: "/",
+        port: 8080,
+        hot: true,
+        inline: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.fs(x|proj)?$/,
+                use: {
+                    loader: "fable-loader",
+                    options: {
+                        babel: babelOptions,
+                        define: isProduction ? [] : ["DEBUG"],
+                        extra: { optimizeWatch: true }
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: babelOptions
+                },
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                use: isProduction ?
+                    ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        //resolve-url-loader may be chained before sass-loader if necessary
+                        use: ['css-loader', 'sass-loader']
+                    })
+                    : ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*$|$)/,
+                use: ["file-loader"]
+            }
+        ]
+    }
+};
